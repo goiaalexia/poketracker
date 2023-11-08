8d27b6aa67977356f0a1426d415f6f431e51287c
913-Goia-Alexia <lexigoia@gmail.com>
2023-11-08 11:53:27 +0200

    added colours for the theme

--- app/src/main/java/com/example/pokemon_boxes/ui/theme/Color.kt
+++ app/src/main/java/com/example/pokemon_boxes/ui/theme/Color.kt
@@ -2,7 +2,29 @@ package com.example.pokemon_boxes.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val NormalType = Color(181, 183, 166)
+val PsychicType = Color(248,96,178)
+val PoisonType = Color(168, 93, 161)
+val GrassType = Color(140, 216, 80)
+val GroundType = Color(232, 200, 87)
+val IceType = Color(149, 241, 255)
+val FireType = Color(250, 85, 67)
+val RockType = Color(205, 189, 115)
+val DragonType = Color(137, 117, 255)
+val WaterType = Color(92, 171, 252)
+val BugType = Color(195, 210, 31)
+val DarkType = Color(101, 68, 52)
+val FightingType = Color(178, 82, 72)
+val GhostType = Color(122, 117, 220)
+val SteelType = Color(195, 194, 218)
+val FlyingType = Color(121, 164, 255)
+val ElectricType = Color(249, 227, 59)
+val FairyType = Color(249, 173, 255)
+
+val LightGray = Color(214,214,214)
+val DarkGray = Color(153,153,153)
+val Cyan = Color(69, 211, 202)
+
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
